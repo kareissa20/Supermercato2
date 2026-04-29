e73a2baa0a2978cbf994305c38fee8c21a04c9bf
DiegiDitti_cyber <michelenishku02@gmail.com>
2026-04-29 09:30:45 +0200

    parte nishku

--- Supermercato.h
+++ Supermercato.h
@@ -2,6 +2,7 @@
 #define SUPERMERCATO_H
 #include <string>
 
+
 void inserisciVendite(int vendite[3][7]);
 void visualizzaVendite(int vendite[3][7]);
 int totaleVenditeProdotto(int vendite[3][7], int prodotto);
